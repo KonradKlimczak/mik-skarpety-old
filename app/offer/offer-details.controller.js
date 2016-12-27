df5b0ecb9b62ff16bb685f11f30855f26dd022a3
Konrad Klimczak <Konrad Klimczak>
2016-12-27 17:23:11 +0100

    Colors added, templates improved

--- app/offer/offer-details.controller.js
+++ app/offer/offer-details.controller.js
@@ -1,8 +1,8 @@
 "use strict";
 angular.module('mikApp').controller('OfferDetailsController', ['$scope', 'OfferService', '$stateParams',
   function ($scope, OfferService, $stateParams) {
-    OfferService.getOffer().success(function (data) {
-      var socks = data.socks;
+    OfferService.getOffer().then(function (data) {
+      var socks = data.data.socks;
 
       for (var i = 0; i < socks.length; i += 1) {
         if (socks[i].id === $stateParams.offerId) {
@@ -10,7 +10,7 @@ angular.module('mikApp').controller('OfferDetailsController', ['$scope', 'OfferS
           break;
         }
       }
-    }).error(function (data, status, headers, config) {
-        console.log('error');
-      });;
+    }, function (data, status, headers, config) {
+      console.log('error');
+    });
   }]);
--- app/offer/offer.controller.js
+++ app/offer/offer.controller.js
@@ -1,24 +1,38 @@
 "use strict";
-angular.module('mikApp').controller('OfferController', ['$scope', 'OfferService', function ($scope, OfferService) {
-  $scope.socks = [];
-  OfferService.getOffer().success(function(data) {
-        $scope.socks = data.socks;
-    }).error(function(data, status, headers, config) {
-        console.log('error');
-    });;
-  $scope.categories = [{
-    "name": "Men",
-    "value": true
-  }, {
-    "name": "Women",
-    "value": true
-  }, {
-    "name": "Sport",
-    "value": true
-  }];
-  $scope.goToDetails = goToDetails;
+angular.module('mikApp').controller('OfferController', [
+  '$scope', 'OfferService',
+  function ($scope, OfferService) {
+    $scope.socks = [];
+    OfferService.getOffer().then(function (data) {
+      $scope.socks = data.data.socks;
+    }, function (data, status, headers, config) {
+      console.log('error');
+    });
+    $scope.categories = [{
+      "name": "Men",
+      "value": true
+    }, {
+      "name": "Women",
+      "value": true
+    }, {
+      "name": "Sport",
+      "value": true
+    }, {
+      "name": "Frotte",
+      "value": true
+    }, {
+      "name": "Stopki",
+      "value": true
+    }, {
+      "name": "Bambusowe",
+      "value": true
+    }, {
+      "name": "Category_7",
+      "value": true
+    }];
+    $scope.goToDetails = goToDetails;
 
-  function goToDetails(sock) {
-    OfferService.goToDetails(sock);
-  }
-}]);
+    function goToDetails(sock) {
+      OfferService.goToDetails(sock);
+    }
+  }]);
