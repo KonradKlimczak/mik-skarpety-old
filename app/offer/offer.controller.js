854c92ac2917b58907c0b0d6c01ff301ef4531f6
Konrad Klimczak <Konrad Klimczak>
2017-01-01 17:45:48 +0100

    Photos added.

--- app/offer/offer.controller.js
+++ app/offer/offer.controller.js
@@ -3,36 +3,17 @@ angular.module('mikApp').controller('OfferController', [
   '$scope', 'OfferService',
   function ($scope, OfferService) {
     $scope.socks = [];
-    OfferService.getOffer().then(function (data) {
-      $scope.socks = data.data.socks;
-    }, function (data, status, headers, config) {
-      console.log('error');
-    });
-    $scope.categories = [{
-      "name": "Men",
-      "value": true
-    }, {
-      "name": "Women",
-      "value": true
-    }, {
-      "name": "Sport",
-      "value": true
-    }, {
-      "name": "Frotte",
-      "value": true
-    }, {
-      "name": "Stopki",
-      "value": true
-    }, {
-      "name": "Bambusowe",
-      "value": true
-    }, {
-      "name": "Category_7",
-      "value": true
-    }];
-    $scope.goToDetails = goToDetails;
+    $scope.categories = OfferService.getCategories();
+    loadOffer();
 
-    function goToDetails(sock) {
-      OfferService.goToDetails(sock);
+    function loadOffer() {
+      OfferService.getOffer().then(function (data) {
+        $scope.socks = data.data.socks;
+      }, function (data) {
+        console.error(data);
+        OfferService.showErrorDialog().then(function() {
+          loadOffer();
+        });
+      });
     }
   }]);
--- app/offer/offer.service.js
+++ app/offer/offer.service.js
@@ -1,30 +1,55 @@
-angular.module('mikApp').service('OfferService', ['$http', '$state', '$location', function ($http, $state, $location) {
-  this.getOffer = getOffer;
-  this.goToDetails = goToDetails;
-  this.getOfferById = getOfferById;
+angular.module('mikApp').service('OfferService', ['$http', '$mdDialog',
+  function ($http, $mdDialog) {
+    this.getOffer = getOffer;
+    this.getOfferById = getOfferById;
+    this.getCategories = getCategories;
+    this.showErrorDialog = showErrorDialog;
 
-  function getOffer() {
-    // This function is going to use $http
-    return $http.get('/app/offer/socks.json');
-  }
-
-  function goToDetails(sock) {
-    // $state.go('details', {param1: sock.id});
-    $location.path('/details/' + sock.id).replace();
-  }
-
-  function getOfferById(sockId) {
-    // This function is going to use $http
-    var socks = getOffer();
-    var sock;
-
-    for (var i = 0; i < socks.length; i += 1) {
-      if (socks[i].id === sockId) {
-        var sock = socks[i];
-        break
-      }
+    function getOffer() {
+      return $http.get('/app/offer/socks.json');
     }
 
-    return sock;
-  }
-}]);
+    function getCategories() {
+      return [{
+        "name": "All",
+        "value": true
+      }, {
+        "name": "Men",
+        "value": true
+      }, {
+        "name": "Women",
+        "value": true
+      }, {
+        "name": "Sport",
+        "value": true
+      }, {
+        "name": "Frotte",
+        "value": true
+      }, {
+        "name": "Stopki",
+        "value": true
+      }, {
+        "name": "Bambusowe",
+        "value": true
+      }, {
+        "name": "Category_7",
+        "value": true
+      }];
+    }
+
+    function getOfferById(sockId) {
+      throw {
+        name: "NotImplementedError",
+        message: "This function is not implemented due to missing backend of app."
+      };
+    }
+
+    function showErrorDialog() {
+      return $mdDialog.show($mdDialog.confirm({
+        title: 'Request failed',
+        textContent: 'HTTP request has failed. Please try again later.',
+        ok: 'Try Again',
+        cancel: 'Cancel'
+      }));
+    }
+  }]);
