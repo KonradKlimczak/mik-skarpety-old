5f253b5da659e4edbf7f912f627fbfdcede5ed0a
jsdevadi <jsdevadi@gmal.com>
2017-01-07 16:51:34 +0100

    Add gulp tasks: copy-css, copy-json, copy-js.

--- gulpfile.js
+++ gulpfile.js
@@ -6,47 +6,38 @@ var gulp = require('gulp'),
     gp_clean = require('gulp-clean'),
     env = require('./environment');
 
+gulp.task('copy-translations', function () {
+  gulp.src("app/translations/**/*.json")
+    .pipe(gulp.dest('dist/translations/'))
+});
+
 gulp.task('copy-templates', function () {
   var rename = require("gulp-rename");
   gulp.src("app/**/*.html")
     .pipe(rename(function(path){
       path.dirname = path.dirname.replace("\\views", "");
     }))
-    .pipe(gulp.dest('dist/template/'))
+    .pipe(gulp.dest('dist/templates/'))
 });
 
-
-gulp.task('templates-fef', function () {
-    var regex = new RegExp("\/app\/(\w)\/views\/g"),
-        filesToReplace = env.appJs.concat(env.template);
-    console.log(filesToReplace);
-    console.log(env.template);
-    for (var i = 0; i < filesToReplace.length; i += 1) {
-        gulp.src(filesToReplace[i])
-            .pipe(gp_replace(regex, '/dist/$1/'))
-            .pipe(gulp.dest('dist/' + filesToReplace[i]));
-    }
+gulp.task('copy-js', function () {
+  var rename = require("gulp-rename");
+  gulp.src("app/**/*.js")
+    .pipe(gp_concat('concated'))
+    .pipe(gp_replace("/app/translations/", "/dist/translations/"))
+    .pipe(gp_replace("/views/", "/"))
+    .pipe(gp_replace("/app/", "/dist/templates/"))
+    .pipe(gp_rename('lib.min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js-fef', function () {
-    var prodJs = env.prodJs;
-    for (var i = 0; i < prodJs.length; i += 1) {
-        prodJs[i] = 'dist/' + prodJs[i];
-    }
-
-    gulp.src(prodJs)
-        .pipe(gp_concat('concated'))
-        .pipe(gp_rename('lib.min.js'))
-        .pipe(gp_uglify())
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('css-fef', function () {
+gulp.task('copy-css', function () {
     return gulp.src(env.css)
         .pipe(gp_concat('lib.min.css'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-app', ['templates-fef', 'js-fef', 'css-fef'], function () { });
+gulp.task('build-app', ['copy-templates', 'copy-js', 'copy-css', 'copy-translations'], function () { });
 
 gulp.task('default', function () { });
