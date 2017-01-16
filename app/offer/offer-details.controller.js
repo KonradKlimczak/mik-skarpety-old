24cd7d7bc3a0f5b06389a22b67e1f49ff1efee99
jsdevadi <jsdevadi@gmal.com>
2017-01-16 21:16:01 +0100

    [adi]Add arrows to change sock in detailed offer.

--- app/offer/offer-details.controller.js
+++ app/offer/offer-details.controller.js
@@ -1,12 +1,21 @@
 "use strict";
 angular.module('mikApp').controller('OfferDetailsController', ['$scope', 'OfferService', '$stateParams',
   function ($scope, OfferService, $stateParams) {
+    $scope.beforeSelectedSock = null;
+    $scope.afterSelectedSock = null;    
+
     OfferService.getOffer().then(function (data) {
       var socks = data.data.socks;
 
       for (var i = 0; i < socks.length; i += 1) {
         if (socks[i].id === $stateParams.offerId) {
           $scope.sock = socks[i];
+          if(i !== 0) {
+            $scope.beforeSelectedSock = socks[i-1];
+          }
+          if(i !== socks.length-1) {
+            $scope.afterSelectedSock = socks[i+1];
+          }
           break;
         }
       }
